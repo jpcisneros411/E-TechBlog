e1af8aa3a2e6eb1463f9760a1414a1c624dcfe45
Monet Man <88108211+jpcisneros411@users.noreply.github.com>
2022-02-27 17:31:48 -0600

    change to homeroute

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -27,32 +27,17 @@ router.get('/', async (req, res) => {
     console.log(err);
     res.status(500).json(err);
   }
+
+  
 });
 
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
 
-
-// GET one painting
-// Use the custom middleware before allowing the user to access the painting
-// router.get('/painting/:id', withAuth, async (req, res) => {
-//   try {
-//     const dbPaintingData = await Painting.findByPk(req.params.id);
-
-//     const painting = dbPaintingData.get({ plain: true });
-
-//     res.render('painting', { painting, loggedIn: req.session.loggedIn });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
-
-// router.get('/login', (req, res) => {
-//   if (req.session.loggedIn) {
-//     res.redirect('/');
-//     return;
-//   }
-
-//   res.render('login');
-// });
+  res.render('login');
+});
 
 module.exports = router;
